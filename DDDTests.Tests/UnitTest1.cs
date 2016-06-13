e5225e4dd850e2676950c2f45d9669dec2342834
Rodrigo Alexandre Ceschi <rodrigoalexandre.sc@gmail.com>
2016-06-13 10:27:26 -0300

    Start

---
+++ DDDTests.Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DDDTests.Domain;
+
+namespace DDDTests.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAbrirOrcamento()
+        {
+            var orcamento = new Orcamento("Rodrigo Alexandre", 100.00m, 3);
+
+            Assert.IsNotNull(orcamento.DataDeAbertura);
+            Assert.IsNull(orcamento.DataDeAprovacao);
+            Assert.AreNotEqual(orcamento.Id, 0);
+            Assert.IsNotNull(orcamento.Cliente);
+        }
+    }
+}
---
+++ DDDTests/Domain/Orcamento.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDDTests.Domain
+{
+    public class Orcamento
+    {
+        public int Id { get; private set; }
+
+        public DateTime DataDeAbertura { get; private set; }
+
+        public string Cliente { get; private set; }
+
+        public DateTime? DataDeAprovacao { get; private set; }
+
+        public int Validade { get; private set; }
+
+        public decimal ValorTotal { get; private set; }
+
+        public Orcamento(string cliente, decimal valorTotal, int validade)
+        {
+            if (string.IsNullOrEmpty(cliente))
+                throw new Exception("Informe o cliente");
+
+            if (valorTotal <= 0)
+                throw new Exception("Informe o valor do orçamento");
+
+            if (validade <= 0)
+                throw new Exception("Validade em dias deve ser maior que 0");
+
+            Id = 0;
+            Cliente = cliente;
+            ValorTotal = valorTotal;
+            DataDeAbertura = DateTime.Now;
+            Validade = validade;
+        }
+
+        private int GerarId()
+        {
+            return new Random().Next();
+        }
+
+        public void Fechar()
+        {
+            if (DataDeAprovacao.HasValue)
+                throw new Exception("Orçamento já aprovado em " + DataDeAprovacao.Value.ToString("d"));
+
+            DataDeAprovacao = DateTime.Now;
+        }
+    }
+}
